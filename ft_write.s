0aa67891d721dd9f2c8642a3e141921ebbdb37ae
Josefig <josefiggaming@gmail.com>
2021-04-28 21:22:54 +0100

    ft_write errno attempt

--- ft_write.s
+++ ft_write.s
@@ -1,6 +1,16 @@
 				global		_ft_write
+				extern		___error
 
 				section		.text
-_ft_write		mov			rax, 0x2000004
+_ft_write:
+				mov			rax, 0x2000004
 				syscall
+				cmp			rax, 0
+				jl			error
+				ret
+error:
+				mov			r8, rax
+				call		___error
+				mov			[rax], r8
+				mov			rax, r8
 				ret
